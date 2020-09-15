414246f2a9df1e412afe78933ea495276f9ca851
homer-crypto <homer.nde-tsakou@polymtl.ca>
2020-09-15 22:54:28 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -13,7 +13,7 @@ def is_even_len(string: str) -> bool:
 
 def remove_third_char(string: str) -> str:
     for string in range(0, 3, 1):
-        print(string{})
+        print(string[])
     pass
 
 
