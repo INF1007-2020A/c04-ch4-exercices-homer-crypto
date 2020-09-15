090abc3c3c7ce6e3f30fc34f4cf63285a96e3a2f
homer-crypto <homer.nde-tsakou@polymtl.ca>
2020-09-15 23:31:13 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -18,7 +18,7 @@ def remove_third_char(string: str) -> str:
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
     for c in string:
-        
+        c += 1
     pass
         
 
