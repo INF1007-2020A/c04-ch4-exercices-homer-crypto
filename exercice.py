ef04ad450387b3ad6e4eb9e18f5869227c4675cb
homer-crypto <homer.nde-tsakou@polymtl.ca>
2020-09-15 23:19:06 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -20,6 +20,7 @@ def replace_char(string: str, old_char: str, new_char: str) -> str:
     if old_char != new_char:
         for old_char in string:
             old_char = new_char
+            
 
     return string
 
