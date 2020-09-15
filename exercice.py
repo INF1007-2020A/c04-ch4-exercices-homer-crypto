a1a2297117527cda788557d9fc8afa380df6c984
homer-crypto <homer.nde-tsakou@polymtl.ca>
2020-09-15 23:47:21 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -25,7 +25,8 @@ def replace_char(string: str, old_char: str, new_char: str) -> str:
 
 def get_number_of_char(string: str, char: str) -> int:
     for i in range(len(string)):
-        if string[]
+        if string[i] == char:
+            
     pass
 
 
